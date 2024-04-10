a4ae26b7c316e50ec409380cbae68d6d03ec1097
lephuockhai <lephuockhai2015@gmail.com>
2024-04-10 20:05:41 +0700

    Config Mui Theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
-
+import Typography from '@mui/material/Typography'
 function App() {
 
   return (
     <>
       <div>KhaxiDev</div>
+
+      <Typography variant='body2' color='text.secondary'> test Typography</Typography>
+
       <Button variant='text'>Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color='success'>Contained</Button>
       <Button variant='outlined'>Outlined</Button>
       <br/>
       <HomeIcon/>

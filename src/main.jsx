04215eeb7a183c57826f3218e8123de4cac6e0ef
lephuockhai <lephuockhai2015@gmail.com>
2024-05-03 23:36:04 +0700

    Add notification

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme.js'
 
+//cấu hình react-toátify hiển thị thông báo trang
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css' // nhảy vào đường dẫn này bên trong node-modules
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
+      <ToastContainer />
     </CssVarsProvider>
   </React.StrictMode>
 )

b25841f9c8d104216abdb8720f8887ed6071f274
lephuockhai <lephuockhai2015@gmail.com>
2024-05-08 22:30:49 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const API_ROOT = 'http://localhost:8017'
 let apiRoot = ''
-
+//có thể sử dụng import.meta.env thay vi dung process.env
 console.log('import.meta.env::', import.meta.env)
 console.log('process.env::', process.env)
 if (process.env.BUILD_MODE === 'dev') {

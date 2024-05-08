6d285f9a2b78a07575214b65774fdafe6002fd89
lephuockhai <lephuockhai2015@gmail.com>
2024-05-08 22:07:27 +0700

    fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
 // export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-pndh.onrender.com'
+let apiRoot = ''
+
+console.log('import.meta.env::', import.meta.env)
+console.log('process.env::', process.env)
+if (process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'http://localhost:8017/'
+}
+if (process.env.BUILD_MODE === 'production') {
+    apiRoot = 'https://trello-api-pndh.onrender.com'
+}
+export const API_ROOT = apiRoot
